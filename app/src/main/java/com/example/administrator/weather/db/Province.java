38863fbf4e4eece80f60d440a207dc650bd051e7
Your Name <979876333@qq.com>
2018-01-29 03:31:38 +0800

    实现遍历省市县三级列表的功能

--- app/src/main/java/com/example/administrator/weather/db/Province.java
+++ app/src/main/java/com/example/administrator/weather/db/Province.java
@@ -2,13 +2,12 @@ package com.example.administrator.weather.db;
 
 import org.litepal.crud.DataSupport;
 
-/**
- * Created by Administrator on 2018/1/28.
- */
+public class Province extends DataSupport {
 
-public class Province extends DataSupport{
     private int id;
+
     private String provinceName;
+
     private int provinceCode;
 
     public int getId() {
